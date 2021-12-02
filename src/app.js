c4485f30e57618a797d52b3402ef6e533c7b40b3
Holly Ford <holly_ford@hotmail.com>
2021-12-02 17:15:05 +0000

    added find by author function

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 require("./db/connection")
 const yargs = require("yargs")
-const { addBook, listBooks } = require("./book/bookMethods")
+const { addBook, listBooks, deleteBook, findOne, findMany } = require("./book/bookMethods")
 
 const app = async (args) => {
     switch (process.argv[2]) {
         case "add":
-            addBook({ author: args.author, title: args.title })
+            addBook({ author: args.author, title: args.title });
             break;
         case "list":
             listBooks();
             break;
+        case "delete":
+            deleteBook({title: args.title});
+            break;
+        case "list one":
+            findOne(process.argv[3]);
+            break;
+        case "find by author":
+            findMany(process.argv[3]);
+            break;
         default:
-            console.log("Incorrect command")
+            console.log("Incorrect command");
             break;
     }
 }
--- src/book/bookMethods.js
+++ src/book/bookMethods.js
@@ -17,3 +17,27 @@ exports.listBooks = async () => {
         console.log(error)
     }
 }
+// find one by title:
+exports.findOne = async (bookObj) => {
+    try {
+        console.log(await Book.findOne({title: bookObj}))
+    } catch (error) {
+        console.log(error)
+    }
+}
+// find many by author
+exports.findMany = async (bookObj) => {
+    try {
+        console.log(await Book.find({author: bookObj}))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// exports.deleteBook = async (bookObj) => {
+//     try {
+//         await Book.deleteOne({title: args.title})
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
